4c6a92eb45e95b76a3bff01bd149634eea6b9823
carlloshen <krlos.henrique98@gmail.com>
2024-02-24 01:58:14 -0300

    feat - estilo e comandos navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,13 @@
 import { HiOutlineMenu } from "react-icons/hi";
 import { FaCartShopping } from "react-icons/fa6";
+import { IoMdClose } from "react-icons/io";
 import icone from "../../assets/images/icone-4x.png";
 import InputHeader from "../inputHeader/InputHeader";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useState } from "react";
 function Header() {
   const navigator = useNavigate()
+  const {pathname} = useLocation()
 
   const [showMenuHamburguer, setShowMenuHamburguer] = useState(false)
 
@@ -16,28 +18,44 @@ function Header() {
   const handleClickPageChange = (element) =>{
     const PAGE_TARGET = element.target.id
     navigator(`${PAGE_TARGET }`)
+    setShowMenuHamburguer(false)
   }
+
+  const links = [
+    { id: "", text: "Home"},
+    { id: "produtos", text: "Produtos" },
+    { id: "categorias", text: "Categorias" },
+    { id: "meuspedidos", text: "Meus Pedidos" }
+  ];
 
 
   return (
     <nav className="bg-blue-900 items-center pt-5 pb-5 flex flex-col">
       <div className="flex justify-between items-center h-20 pl-5 pr-5 w-screen flex-wrap">
-        <button  onClick={() =>handleShowMenuHamburguer(true)}>
+        <button className="md: hidden"  onClick={() =>handleShowMenuHamburguer(true)}>
           <HiOutlineMenu className="text-white text-2xl" />
         </button>
         <div className={`z-10 w-screen h-screen fixed top-0  bg-slate-100 transition-all duration-300 ease-in-out  ${showMenuHamburguer ? "left-0 " :" left-[-100vw]"}`}>
-          <div className="ml-5 mr-5 mt-10">
-            <p onClick={() =>handleShowMenuHamburguer(false)}>X</p>
+          <div className="ml-5 mr-5 mt-5">
+            <button className="mb-2" onClick={() =>handleShowMenuHamburguer(false)}>
+              <IoMdClose/>
+            </button>
             <hr className="mb-3" />
             <nav className="flex flex-col mb-10">
-              <a id="" className="text-stone-500 hover:bg-slate-300 p-2 rounded-md" onClick={(element) => handleClickPageChange(element)}>Home</a>
-              <a id="produtos" className="text-stone-500 hover:bg-slate-300 p-2 rounded-md" onClick={(element) => handleClickPageChange(element)}>Produtos</a>
-              <a id="categorias" className="text-stone-500 hover:bg-slate-300 p-2 rounded-md" onClick={(element) => handleClickPageChange(element)}>Categorias</a>
-              <a id="meuspedidos" className="text-stone-500 hover:bg-slate-300 p-2 rounded-md" onClick={(element) => handleClickPageChange(element)}>Meus Pedidos</a>
+              {links.map((link, index) =>(
+                 <a 
+                  key={index}
+                  id={link.id}
+                  className={`${pathname === "/" + link.id ? "text-orange-500" : "text-stone-500"} font-semibold hover:bg-slate-300 p-2 rounded-md`}
+                  onClick={(link) => handleClickPageChange(link)}>{link.text}
+                 </a>
+              ))}
             </nav>
             <hr className="mb-3" />
             <div className="flex justify-around">
-              <button className="text-stone-500 font-semibold ">Cadastre-se</button>
+              <button id="cadastro" className="text-stone-500 font-semibold" onClick={(element) => handleClickPageChange(element)}>
+                Cadastre-se
+              </button>
               <button className="bg-blue-700 rounded-md text-white w-[106px] h-[30px] text-sm front-bold">
                 Entrar
               </button>
