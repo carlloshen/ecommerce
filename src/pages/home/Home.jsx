6fbce4aff6d8bddbbd777c7e61248c2b381f0f68
carlloshen <krlos.henrique98@gmail.com>
2024-02-27 00:06:54 -0300

    Feat - Images

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,13 @@
 import Card from "../../components/card/Card";
 import PromocaoMobile from "../../assets/images/promocao-mobile.png";
-import PromocaoDesktop from "../../assets/images/promocao-desktop.png";
+import PromocaoDesktop from "../../assets/images/banner-desktop-2.png";
 
 function Home() {
   return (
     <>
       <main className="mb-10 ">
         <div className="relative flex flex-col items-center">
-          <img src={PromocaoMobile} alt="" className="md:hidden w-full" />
+          <img src={PromocaoDesktop} alt="" className="md:hidden w-full" />
           <img src={PromocaoDesktop} alt="" className="hidden md:block" />
           <button className= "md:hidden bg-orange-500 text-white w-[80vw] h-[40px] absolute bottom-8 font-semibold rounded-md hover:bg-orange-600">Aproveitar a Oferta</button>
         </div>
