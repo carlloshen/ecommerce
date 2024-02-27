8a19ba463c6ea8aacc4e6010d9259bad614d7319
carlloshen <krlos.henrique98@gmail.com>
2024-02-26 21:22:39 -0300

    Fix/NotFound

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -7,9 +7,9 @@ function NotFound() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold text-gray-800">404 - Página não encontrada</h1>
-      <p className="text-lg text-gray-600 mt-4">Desculpe, a página que você está procurando não pôde ser encontrada.</p>
+    <div className="flex flex-col items-center justify-center h-screen w-screen text-center">
+      <h1 className="text-4xl font-bold text-gray-800 text-center">404 - Página não encontrada</h1>
+      <p className="text-lg text-gray-600 mt-4 text-center">Desculpe, a página que você está procurando não pôde ser encontrada.</p>
       <button onClick={goToHomePage} className="mt-8 bg-blue-900 hover:bg-blue-950 text-white font-bold py-2 px-4 rounded">
         Ir para a página inicial
       </button>
