22d56133648ccc7d23146a79b5d188714f3e9127
carlloshen <krlos.henrique98@gmail.com>
2024-02-27 11:35:55 -0300

    Feat - Pagina produto especifico

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,6 +4,7 @@ import LoginECadastro from "../pages/loginECadastro/LoginECadastro";
 import MeusPedidos from "../pages/meusPedidos/MeusPedidos";
 import Categorias from "../pages/categorias/Categorias"
 import Home from "../pages/home/Home";
+import Product from "../components/product/Product";
 
 function Router() {
   return (
@@ -13,7 +14,7 @@ function Router() {
         <Route path="/login" element={<LoginECadastro />} />
         <Route path="/cadastro" element={<LoginECadastro />} />
         <Route path="/produtos" element="produtos" />
-        <Route path="/produtos/:id" element="produta" />
+        <Route path="/produtos/:id" element={<Product/>} />
         <Route path="/categorias" element={<Categorias/>} />
         <Route path="/meusPedidos" element={<MeusPedidos />} />
         <Route path="*" element={<NotFound />} />
