54913e1faa0bf1158dc9e17af3d12ed3d7eaacbe
carlloshen <krlos.henrique98@gmail.com>
2024-02-28 16:16:07 -0300

    Feat - produto mobile e desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import { useLocation } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Router from "./router/Router";
 
 function App() {
+
+  const {pathname} = useLocation()
+
   return (
     <>
-      <Header/>
-      <Router/>
-      <Footer/>
+      <Header />
+      <div className={pathname === "/login" ? null : pathname === "/cadastro" ? null : "min-h-[50vh]"}>
+        <Router />
+      </div>
+      <Footer />
     </>
   );
 }
--- src/pages/produtos/Produtos.jsx
+++ src/pages/produtos/Produtos.jsx
@@ -1,14 +1,50 @@
+import Card from "../../components/card/Card";
+
 function Produtos() {
   return (
     <>
       <section className=" w-screen mb-10 mt-5 flex justify-center ">
-        <div className="w-[90vw]">
+        <div className="w-[90vw] lg:flex lg:flex-row">
           <div className="text-center lg:hidden">
-            <select name="categorias" id="categorias"  className=" w-[250px]  pr-5 h-[48px] appearance-none text-center font-semibold text-white bg-orange-500 rounded-md">
-              <option value="computadores" >Computadores</option>
+            <select
+              name="categorias"
+              id="categorias"
+              className=" w-[250px]  pr-5 h-[48px] appearance-none text-center font-semibold text-white bg-orange-500 rounded-md"
+            >
+              <option value="computadores">Computadores</option>
               <option value="computadores">Celulares</option>
               <option value="computadores">Computadores</option>
             </select>
+          </div>
+          <div className="hidden lg:block">
+            <div className="shadow-lg flex flex-col p-5 gap-5 w-[251px] text-stone-500 font-semibold">
+              <div className="flex gap-2 ">
+                <input type="radio" id="" name="computador" value="" checked/>
+                <label htmlFor="computador"> Computador</label>
+              </div>
+              <div className="flex gap-2">
+                <input type="radio" id="" name="computador" value="" />
+                <label htmlFor="computador"> Computador</label>
+              </div>
+              <div className="flex gap-2">
+                <input type="radio" id="" name="computador" value="" />
+                <label htmlFor="computador"> Computador</label>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-wrap items-center w-full gap-2 md:gap-5 justify-center ">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
           </div>
         </div>
       </section>
