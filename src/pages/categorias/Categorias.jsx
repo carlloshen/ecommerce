b9e753725768291cf7bb325b668ec4330549dd0b
carlloshen <krlos.henrique98@gmail.com>
2024-03-01 19:13:48 -0300

    alteracao categorias

--- src/pages/categorias/Categorias.jsx
+++ src/pages/categorias/Categorias.jsx
@@ -1,12 +1,12 @@
 import image from "../../assets/images/tenis.png";
 function CategoryCard({categoryName}) {
   return (
-    <div className="grid grid-cols-3 lg:grid-cols-4 bg-slate-100  h-[96px] rounded-md pl-3 shadow-md">
-      <div className="col-span-1 flex justify-center items-center">
+    <div className="flex bg-slate-100 w-[250px]  h-[96px] rounded-md pl-2 shadow-md">
+      <div className="mr-2 flex items-center">
         <img src={image} alt="" className=" w-[82px] h-[81px] rounded-md shadow-xl" />
       </div>
-      <div className="col-span-2 flex justify-center items-center">
-        <p className="text-xl font-semibold">{categoryName}</p>
+      <div className="flex items-center">
+        <p className="text-[19px] font-semibold">{categoryName}</p>
       </div>
     </div>
   );
@@ -15,8 +15,8 @@ function CategoryCard({categoryName}) {
 function Categorias() {
   return (
     <section className=" w-screen mb-20 mt-20 flex justify-center">
-      <div className="w-[90vw]">
-        <div className="flex flex-wrap items-center w-full gap-4 md:gap-5 justify-center ">
+      <div className="md:w-[90vw] ">
+        <div className="flex flex-wrap items-center w-full gap-3 md:gap-4 justify-center md:justify-start max-w-[1200px]">
           <CategoryCard categoryName={"Computadores"} />
           <CategoryCard categoryName={"Processadores"} />
           <CategoryCard categoryName={"Placa-mãe"} />
